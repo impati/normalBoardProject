a8e74cb4561b61a146a970e3b6f40e1c120da194
impati <impati0716@icloud.com>
2023-01-02 18:11:54 +0900

    document : 도메인 ERD 다이어그램 작성

---
+++ normalBoard/src/main/java/com/example/normalboard/domain/Article.java
@@ -0,0 +1,8 @@
+package com.example.normalboard.domain;
+
+public class Article extends BaseEntity{
+    private Long id;
+    private String title;
+    private String content;
+    private String hashtag;
+}
---
+++ normalBoard/src/main/java/com/example/normalboard/domain/BaseEntity.java
@@ -0,0 +1,9 @@
+package com.example.normalboard.domain;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class BaseEntity {
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+}
