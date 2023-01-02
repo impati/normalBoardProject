bc8e3fe54986001b9c3d310f3e52320e09af5528
impati <impati0716@icloud.com>
2023-01-02 17:21:47 +0900

    feat : 스프링 부트 프로젝트 시작

---
+++ normalBoard/src/main/java/com/example/normalboard/NormalBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.normalboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalBoardApplication.class, args);
+    }
+
+}
---
+++ normalBoard/src/test/java/com/example/normalboard/NormalBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.normalboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NormalBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
