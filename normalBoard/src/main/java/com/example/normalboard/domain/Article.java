efac5805f4bfb4f0eafb2c4bcca09de0a588d7f7
impati <impati0716@icloud.com>
2023-01-03 17:32:59 +0900

    feat : 도메인코드를 엔티티로 변환

--- normalBoard/src/main/java/com/example/normalboard/domain/Article.java
+++ normalBoard/src/main/java/com/example/normalboard/domain/Article.java
@@ -1,8 +1,66 @@
 package com.example.normalboard.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+@Getter
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class Article extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false,length = 10000)
     private String content;
+
     private String hashtag;
+
+
+    @OneToMany(mappedBy = "article",cascade = CascadeType.REMOVE)
+    @ToString.Exclude
+    private Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+
+    public void updateContent(String content){
+        this.content = content ;
+    }
+
+
+    public static Article of(String title , String content){
+        Article article = new Article();
+        article.title = title;
+        article.content = content;
+        return article;
+    }
+
+    public static Article of(String title , String content,String hashtag){
+        Article article = of(title, content);
+        article.hashtag = hashtag;
+        return article;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- normalBoard/src/main/java/com/example/normalboard/domain/ArticleComment.java
+++ normalBoard/src/main/java/com/example/normalboard/domain/ArticleComment.java
@@ -1,7 +1,53 @@
 package com.example.normalboard.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Table(indexes = {
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt")
+
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class ArticleComment extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
     private Article article;
+
+    @Column(nullable = false,length = 500)
     private String content;
+
+
+    private ArticleComment(Article article, String content) {
+        this.article = article;
+        this.content = content;
+    }
+
+    public static ArticleComment of(Article article , String content){
+        return new ArticleComment(article,content);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ArticleComment)) return false;
+        ArticleComment that = (ArticleComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
