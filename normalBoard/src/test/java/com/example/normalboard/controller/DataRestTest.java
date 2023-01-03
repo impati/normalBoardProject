35394a720acf82b4a85d83d9ceb9bc73760cd39d
impati <impati0716@icloud.com>
2023-01-03 20:33:22 +0900

    test : spring data rest 테스트

---
+++ normalBoard/src/test/java/com/example/normalboard/controller/DataRestTest.java
@@ -0,0 +1,39 @@
+package com.example.normalboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("Data Rest Test")
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+
+    @DisplayName("[api] 게시글 리스트 조회")
+    @Test
+    public void givenNoting_whenRequestingArticles_thenReturnsArticlesJsonResponse() throws Exception{
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+
+    }
+
+
+
+}
