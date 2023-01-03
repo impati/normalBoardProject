9e391faf5904617e95a1bc6949b614aef0bf4a0d
impati <impati0716@icloud.com>
2023-01-03 22:13:41 +0900

    feat : querydsl 를 이용하여 검색 api 개발

--- normalBoard/src/main/java/com/example/normalboard/repository/ArticleCommentRepository.java
+++ normalBoard/src/main/java/com/example/normalboard/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.example.normalboard.repository;
 
 import com.example.normalboard.domain.ArticleComment;
+import com.example.normalboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment,Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment,Long>,
+        QuerydslPredicateExecutor<ArticleComment>, // 엔티티안에 있는 기본 검색 기능 추가 , exactMatch
+        QuerydslBinderCustomizer<QArticleComment> //
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content,root.createdAt,root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
+
 }
