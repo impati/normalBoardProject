01183c7e63a6364c6961d6a48de3666f20240b1e
impati <impati0716@icloud.com>
2023-01-03 21:26:05 +0900

    feat : 불필요한 통합 테스트 disabled

--- normalBoard/src/test/java/com/example/normalboard/controller/DataRestTest.java
+++ normalBoard/src/test/java/com/example/normalboard/controller/DataRestTest.java
@@ -1,5 +1,6 @@
 package com.example.normalboard.controller;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Disabled("Spring Data Rest 통합 테스트는 불필요하므로 비활성화")
 @DisplayName("Data Rest Test")
 @AutoConfigureMockMvc
 @SpringBootTest
