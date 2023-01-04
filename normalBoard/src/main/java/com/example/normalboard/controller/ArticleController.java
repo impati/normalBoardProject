f003b562d8709ade225bee873dc6934b6c244acd
impati <impati0716@icloud.com>
2023-01-04 17:27:05 +0900

    feat : 게시판 뷰 기본 골격 작성

--- normalBoard/src/main/java/com/example/normalboard/controller/ArticleController.java
+++ normalBoard/src/main/java/com/example/normalboard/controller/ArticleController.java
@@ -1,8 +1,13 @@
 package com.example.normalboard.controller;
 
+import com.example.normalboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 /**
  * /articles
@@ -12,13 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 @RequestMapping("/articles")
+@RequiredArgsConstructor
 public class ArticleController {
 
-
-    @GetMapping("/")
-    public String returnArticlesView(){
-
-        return "";
+    @GetMapping()
+    public String returnArticlesView(Model model){
+        model.addAttribute("articles",new ArrayList<>());
+        return "/articles/index";
     }
 
 
