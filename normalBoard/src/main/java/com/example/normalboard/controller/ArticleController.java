0ed342a619af73fe5cf9a5243215d715264656f2
impati <impati0716@icloud.com>
2023-01-04 20:23:50 +0900

    feat : 게시글 뷰 구현

--- normalBoard/src/main/java/com/example/normalboard/controller/ArticleController.java
+++ normalBoard/src/main/java/com/example/normalboard/controller/ArticleController.java
@@ -1,10 +1,12 @@
 package com.example.normalboard.controller;
 
+import com.example.normalboard.domain.Article;
 import com.example.normalboard.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -24,6 +26,13 @@ public class ArticleController {
     public String returnArticlesView(Model model){
         model.addAttribute("articles",new ArrayList<>());
         return "/articles/index";
+    }
+
+    @GetMapping("/{articleId}")
+    public String returnArticleView(@PathVariable Long articleId, Model model){
+        model.addAttribute("article", Article.of("dummy","dummy content"));
+        model.addAttribute("articleComments", Article.of("dummy","dummy content"));
+        return "/articles/detail";
     }
 
 
