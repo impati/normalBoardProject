03691a5231c71d34b235500b86926ae705386093
impati <impati0716@icloud.com>
2023-01-05 17:59:31 +0900

    test :#11-로그인 테스트 및 기존 테스트에 시큐리티 설정

--- normalBoard/src/test/java/com/example/normalboard/controller/ArticleControllerTest.java
+++ normalBoard/src/test/java/com/example/normalboard/controller/ArticleControllerTest.java
@@ -1,16 +1,19 @@
 package com.example.normalboard.controller;
 
+import com.example.normalboard.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
@@ -31,7 +34,7 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.view().name("/articles/index"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("articles"));
     }
-    @Disabled
+
     @DisplayName("[View] [GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestArticleView_thenReturnArticleVIew() throws Exception{
@@ -60,6 +63,8 @@ class ArticleControllerTest {
                 .andExpect(MockMvcResultMatchers.view().name("/articles/search-hashtag"))
                 .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
     }
+
+
 
 
 }
