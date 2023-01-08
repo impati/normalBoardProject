64550017f493599dd1fdf060230fd740124a0320
impati <impati0716@icloud.com>
2023-01-08 19:52:11 +0900

    refactor : 예제 문자 삭제 및 로그인 테스트 문제 해결

--- normalBoard/src/test/java/com/example/normalboard/controller/AuthControllerTest.java
+++ normalBoard/src/test/java/com/example/normalboard/controller/AuthControllerTest.java
@@ -9,11 +9,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mockMvc;
@@ -28,7 +31,8 @@ public class AuthControllerTest {
     public void givenNothing_whenTryingToLogin_thenReturnLoginVIew() throws Exception{
         mockMvc.perform(MockMvcRequestBuilders.get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andDo(MockMvcResultHandlers.print());
 
     }
 
